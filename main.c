21e51f855891da19994fdf801e5b4d24632bb338
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-04-05 02:37:17 +0700

    Update main.c

--- main.c
+++ main.c
@@ -20,6 +20,67 @@ typedef struct {
     int y;
 } Point;
 
+bool isValid(Point p, int maze[][cols]) {
+    // Check if the point is within the bounds of the maze and has not been visited
+    return p.x >= 0 && p.x < rows && p.y >= 0 && p.y < cols && maze[p.x][p.y] == 1 && !visited[p.x][p.y];
+}
+
+bool solveMaze(Point start, Point end, int maze[][cols]) {
+    // Initialize the visited array to false
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            visited[i][j] = false;
+        }
+    }
+
+    // Create a stack to keep track of the current path
+    Point stack[MAX_SIZE * MAX_SIZE];
+    Point parents[MAX_SIZE][MAX_SIZE] = {0};
+    int top = 0;
+    stack[top++] = start;
+
+    // Loop until the stack is empty or the end point is reached
+    while (top > 0) {
+        Point curr = stack[--top];
+        visited[curr.x][curr.y] = true;
+        if (curr.x == end.x && curr.y == end.y) {
+            // If the end point is reached, print the path
+            int solution[MAX_SIZE][MAX_SIZE] = {0};
+            Point parent = parents[end.x][end.y];
+            while (parent.x != start.x || parent.y != start.y) {
+                solution[parent.x][parent.y] = 1;
+                parent = parents[parent.x][parent.y];
+            }
+            solution[start.x][start.y] = 1;
+
+            // Print the solution as a 2D array
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    if (i == end.x && j == end.y) {
+                        printf("1 ");
+                    } else {
+                        printf("%d ", solution[i][j]);
+                    }
+                }
+                printf("\n");
+            }
+            return true;
+        }
+        // Check all neighbors of the current point
+        Point neighbors[4] = {{curr.x-1, curr.y}, {curr.x+1, curr.y}, {curr.x, curr.y-1}, {curr.x, curr.y+1}};
+        for (int i = 0; i < 4; i++) {
+            Point neighbor = neighbors[i];
+            if (isValid(neighbor, maze)) {
+                stack[top++] = neighbor;
+                parents[neighbor.x][neighbor.y] = curr;
+            }
+        }
+    }
+    // If the stack is empty and the end point was not reached, the maze is unsolvable
+    return false;
+}
+
+
 int main() {
     Point start, end;
     int maze[rows][cols];
@@ -28,9 +89,28 @@ int main() {
     char nama_file[128];
     printf("nama file: ");
     scanf("%s", nama_file);
+    FILE* inputFile = fopen(nama_file, "r");
+    if (inputFile == NULL) {
+        printf("Error: masukan file tidak dapat dibuka!\n");
+        return 1;
+    }
+    fscanf(inputFile, "%d %d", &start.x, &start.y);
+    fscanf(inputFile, "%d %d", &end.x, &end.y);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            char c;
+            fscanf(inputFile, " %c", &c);
+            maze[i][j] = c - '0';
+        }
+    }
+    fclose(inputFile);
     
-    printf("arjuna menemukan cinta!\n");
-    printf("arjuna tidak menemukan cinta!\n");
+    // Solve the maze and print the result
+    if (solveMaze(start, end, maze)) {
+        printf("arjuna menemukan cinta!\n");
+    } else {
+        printf("arjuna tidak menemukan cinta!\n");
+    }
 
     return 0;
 }
